8c2fdf4bbf1c276fdb036dd2c3512628770da1f0
crz21 <2356578958@qq.com>
2025-04-30 00:02:14 +0800

    修改定时器

--- LPUART/app/led.c
+++ LPUART/app/led.c
@@ -16,9 +16,11 @@ void led1_thread(void *arg)
 {
     for (;;) {
         if (xSemaphoreTake(g_test_mutex, portMAX_DELAY)) {
-            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
-            vTaskDelay(pdMS_TO_TICKS(200));
+            // HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+            HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
             xSemaphoreGive(g_test_mutex);
+            vTaskDelay(pdMS_TO_TICKS(200));
+            // delay_ms(100);
         }
     }
 }
@@ -27,16 +29,18 @@ void led2_thread(void *arg)
 {
     for (;;) {
         if (xSemaphoreTake(g_test_mutex, portMAX_DELAY)) {
-            HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-            vTaskDelay(pdMS_TO_TICKS(200));
+            // HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+            HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
             xSemaphoreGive(g_test_mutex);
+            vTaskDelay(pdMS_TO_TICKS(200));
+            // delay_ms(100);
         }
     }
 }
 
 void led_init(void)
 {
+    g_test_mutex = xSemaphoreCreateMutex();
     xTaskCreate(led1_thread, "led1_task", 128, NULL, 5, &m_led1_thread);
     xTaskCreate(led2_thread, "led2_task", 128, NULL, 5, &m_led2_thread);
-    g_test_mutex = xSemaphoreCreateMutex();
 }
