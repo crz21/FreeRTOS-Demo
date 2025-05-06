e5d5d75745d08d86eabc5089233d548c9e2c0a9f
crz21 <2356578958@qq.com>
2025-05-06 10:18:20 +0800

    修改文件名为freertos_demo

--- LPUART/MDK-ARM/LPUART/shell.d
+++
@@ -1,7 +0,0 @@
-lpuart\shell.o: ..\middlewares\letter_shell\shell.c
-lpuart\shell.o: ..\middlewares\letter_shell\shell.h
-lpuart\shell.o: ..\middlewares\letter_shell\shell_cfg.h
-lpuart\shell.o: F:\app\keil\ARM\ARMCC\Bin\..\include\string.h
-lpuart\shell.o: F:\app\keil\ARM\ARMCC\Bin\..\include\stdio.h
-lpuart\shell.o: F:\app\keil\ARM\ARMCC\Bin\..\include\stdarg.h
-lpuart\shell.o: ..\middlewares\letter_shell\shell_ext.h
